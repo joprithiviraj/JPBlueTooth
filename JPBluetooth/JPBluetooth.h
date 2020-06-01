385857312ea8ad2f29c90cf1894feeb4e23a49b7
joprithiviraj <prithiviraj1522@gmail.com>
2020-06-01 17:34:49 +0530

    first commit

---
+++ JPBluetooth/JPBluetooth.h
@@ -0,0 +1,19 @@
+//
+//  JPBluetooth.h
+//  JPBluetooth
+//
+//  Created by Prithiviraj Tamilarasan on 01/06/20.
+//  Copyright © 2020 Prithiviraj Tamilarasan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for JPBluetooth.
+FOUNDATION_EXPORT double JPBluetoothVersionNumber;
+
+//! Project version string for JPBluetooth.
+FOUNDATION_EXPORT const unsigned char JPBluetoothVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JPBluetooth/PublicHeader.h>
+
+
